e2e94dbbea3194092ae94d7ce1fee390b68f032f
AmoghPAdhyapak <amoghpadhyapak@gmail.com>
2026-06-15 20:59:36 +0530

    Delete src directory

--- src/components/common/PageMeta.tsx
+++
@@ -1,5 +0,0 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
-export const AppWrapper = ({ children }: { children: React.ReactNode }) => (
-  <TooltipProvider>{children}</TooltipProvider>
-);
-export default function PageMeta() { return null; }
